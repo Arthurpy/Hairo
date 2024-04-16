b317d8ec7180b420b37eefe307f8431842382394
pierrrebouillard <pierre.bouillard@epitech.eu>
2024-04-16 18:05:43 +0200

    Ajout de fichiers PDF pour les cours de PACES dans les ressources

--- src/vue/ressources.vue
+++ src/vue/ressources.vue
@@ -1,64 +1,63 @@
 <template>
-    <div class="bg-[#F4FBFF] flex">
-        <sidebar :activeButton="'ressources'"/>
-        <div class="flex flex-col">
-            <div class="flex mt-[40px] bg-black w-[70vw] h-[56px] ml-[50px] rounded-lg">
-                <h4> Révisions </h4>
-            </div>
-            <div class="bg-black w-[70vw] h-[91px] rounded-lg mt-[30px] ml-[50px]">
-                que rechercher vous?
-            </div>
-            <p class="text-black ml-[50px] mt-[30px]"> Thème adapté à vos exam</p>
-            <div class="flex flex-row ml-[50px] mt-[10px] h-[273px] space-x-[23px]">
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-            </div>
-            <p class="text-black ml-[50px] mt-[30px]"> Autres thèmes</p>
-            <div class="flex flex-row ml-[50px] mt-[10px] h-[273px] space-x-[23px]">
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-                <div class="bg-[#2176FF] w-[25%] rounded-lg">
-                    theme
-                </div>
-            </div>
-        </div>
+  <div class="bg-[#F4FBFF] flex">
+    <sidebar :activeButton="'ressources'"/>
+    <div class="flex flex-col">
+      <div class="flex mt-[40px] bg-black w-[70vw] h-[56px] ml-[50px] rounded-lg">
+        <h4> Révisions </h4>
+      </div>
+      <div class="bg-black w-[70vw] h-[91px] rounded-lg mt-[30px] ml-[50px] flex items-center text-base p-[15px]">
+        que rechercher vous?
+        <input type="text" v-model="searchQuery" class="ml-[50px] mt-[10px] p-[5px] rounded-lg w-[75%]">
+      </div>
+      <p class="text-black ml-[50px] mt-[30px]"> Thème adapté à vos exam</p>
+      <!-- Divisez la liste des cours en sous-listes de 4 éléments chacune -->
+      <div v-for="(row, rowIndex) in filteredChunkedCourses" :key="rowIndex" class="flex flex-row ml-[50px] mt-[10px] space-x-[23px] h-[273px]">
+        <!-- Parcourez chaque sous-liste pour afficher les cours -->
+        <router-link v-for="(course, index) in row" :key="index" :to="{ name: 'CourseDetails', params: { courseName: course } }" class="bg-[#2176FF] w-[25%] rounded-lg">
+          {{ course }}
+        </router-link>
+      </div>
     </div>
-  </template>
-
-
-
-
+  </div>
+</template>
 
 <script>
-  import sidebar from './../components/sidebar.vue';
+import sidebar from './../components/sidebar.vue';
+import pacesFolders from './../assets/pacesFolders.json';
 
-  export default {
-    name: 'ressources',
-    components: {
-      sidebar,
+export default {
+  name: 'ressources',
+  components: {
+    sidebar,
+  },
+  data() {
+    return {
+      isLightTheme: true,
+      courses: pacesFolders,
+      searchQuery: '',
+    };
+  },
+  computed: {
+    chunkedCourses() {
+      const chunkSize = 4;
+      const chunkedArray = [];
+      for (let i = 0; i < this.courses.length; i += chunkSize) {
+        chunkedArray.push(this.courses.slice(i, i + chunkSize));
+      }
+      return chunkedArray;
     },
-    data() {
-      return {
-        isLightTheme: true,
-      };
+    filteredCourses() {
+      const query = this.searchQuery.toLowerCase();
+      return this.courses.filter(course => course.toLowerCase().includes(query));
     },
-    }
-
+    filteredChunkedCourses() {
+      const chunkSize = 4;
+      const chunkedArray = [];
+      for (let i = 0; i < this.filteredCourses.length; i += chunkSize) {
+        chunkedArray.push(this.filteredCourses.slice(i, i + chunkSize));
+      }
+      return chunkedArray;
+    },
+  },
+};
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { copy } from 'vite-plugin-copy';
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    copy({
-      targets: [
-        { src: 'src/assets/**/*', dest: 'back/Hairo_Back/static/dist/assets' }
-      ],
-      hook: 'writeBundle' // Optionnel: spécifiez quand la copie doit avoir lieu
-    })
-  ],
-  build: {
-    outDir: 'back/Hairo_Back/static/dist',
-    emptyOutDir: true, // Assurez-vous que ce paramètre correspond à vos besoins
-    // Vos autres configurations de build ici...
-  }
+  plugins: [vue()]
 });
