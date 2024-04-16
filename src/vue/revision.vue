2689c2e966bc274f839fb65f2c93d2f8fe3096b1
Arthur Py <arthurpy@Arthur-2.local>
2024-04-16 21:18:28 +0200

    add revision

--- src/vue/revision.vue
+++ src/vue/revision.vue
@@ -1,36 +1,58 @@
 <template>
-    <div class="bg-[#D2E3FF] flex">
-        <sidebar :activeButton="'revisions'"/>
-        <div class="flex flex-col ml-80 h-screen">
-            <SearchBar/>
-            <div class="flex mt-[4% ml-[5%] flex-col px-12 mt-5 ">
-                <div class="bg-white text-[#2176FF] w-[100%] rounded-lg">
-                    bon 
-                </div>
-            </div>
-        </div>
+  <div class="bg-blue-200 flex">
+    <sidebar :activeButton="'revisions'" />
+    <div class="flex flex-col ml-80 h-screen">
+      <SearchBar />
+      <div
+        v-for="(revision, index) in myRevision"
+        :key="index"
+        class="flex ml-[5%] flex-col mt-5 bg-white text-[#2176FF] border-b-8 border-r-8 border-[#2176FF] rounded-3xl relative" 
+      >
+          <div class="flex justify-center">
+            <p class="card-header justify-center bg-[#2176FF] rounded-b-xl flex text-white w-48">{{ revision.prog }}</p>
+          </div>
+          <div class="flex items-center">
+            <h3 class="text-xl font-semibold mb-2 card-body mr-96 justify-start">{{ revision.titre }}</h3>
+            <img :src="revision.img_src" alt="Image" class="w-20 h-20 justify-center" />
+          </div>
+          <div class="absolute top-2 right-2">
+            <div
+              :href="revision.red"
+              class="flex hover:text-blue-200 text-white p-2 bg-[#2176FF] rounded-full cursor-pointer"
+            >GO</div>
+          </div>
+          <div class="flex m-3 text-black justify-start items-start">
+            <p class="mb-1 mx-11">Temps de formation: {{ revision.temp_form }}</p>
+            <p class="mb-1">Nombre de questions: {{ revision.nbr_qcm }}</p>
+          </div>
+      </div>
     </div>
-  </template>
-
-
-
-
+  </div>
+</template>
 
 <script>
-  import sidebar from './../components/sidebar.vue';
-  import SearchBar from './../components/SearchComponent.vue';
+import sidebar from './../components/sidebar.vue';
+import SearchBar from './../components/SearchComponent.vue';
 
-  export default {
-    name: 'revisions',
-    components: {
-      sidebar,
-      SearchBar,
-    },
-    data() {
-      return {
-        isLightTheme: true,
-      };
-    },
-    }
-
+export default {
+  name: 'revisions',
+  components: {
+    sidebar,
+    SearchBar,
+  },
+  data() {
+    return {
+      myRevision: [
+        {prog: "60%", red: "https://example.com", temp_form: "30 min", nbr_qcm: "15", titre: "Biologie cellulaire", img_src: "src/assets/coeur.png"},
+        {prog: "75%", red: "https://example.com", temp_form: "45 min", nbr_qcm: "20", titre: "Anatomie humaine", img_src: "../anatomy"},
+        {prog: "50%", red: "https://example.com", temp_form: "40 min", nbr_qcm: "12", titre: "Biochimie structurale", img_src: "../biochemistry"},
+        {prog: "80%", red: "https://example.com", temp_form: "50 min", nbr_qcm: "18", titre: "Physiologie humaine", img_src: "../physiology"},
+        {prog: "65%", red: "https://example.com", temp_form: "35 min", nbr_qcm: "14", titre: "Histologie", img_src: "../histology"},
+        {prog: "55%", red: "https://example.com", temp_form: "25 min", nbr_qcm: "10", titre: "Chimie générale", img_src: "../chemistry"},
+        {prog: "70%", red: "https://example.com", temp_form: "50 min", nbr_qcm: "16", titre: "Embryologie", img_src: "../embryology"},
+        {prog: "45%", red: "https://example.com", temp_form: "30 min", nbr_qcm: "12", titre: "Biophysique", img_src: "../biophysics"}
+      ]
+    };
+  },
+};
 </script>
