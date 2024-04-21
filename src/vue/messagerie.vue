c7a8db6fc456f3bea3390de7efdff7b4b1ea3151
Aymteb <ayman@digitaledge.studio>
2024-04-21 19:31:54 +0100

    [ADD] img work in progress

--- src/vue/messagerie.vue
+++ src/vue/messagerie.vue
@@ -1,24 +1,25 @@
 <template>
+  <div class="bg-blue-200 flex">
     <sidebar :activeButton="'messagerie'"/>
-    <div class="bg-blue-200 flex">
-        <div class="flex flex-col ml-80">
-    <div class="flex bg-white text-[#2176FF] w-[70vw] h-[56px] ml-[50px] rounded-lg justify-between items-center mt-10 py-16">
-      <h1 class="search-title text-[#2176FF] text-5xl font-bold flex items-center ml-10">Messagerie</h1>
+    <div class="flex flex-col ml-80 flex-grow">
+      <div class="flex bg-white text-[#2176FF] w-[70vw] h-[56px] ml-[50px] rounded-lg justify-between items-center mt-10 py-16">
+        <h1 class="search-title text-[#2176FF] text-5xl font-bold flex items-center ml-10">Messagerie</h1>
+      </div>
+      <div class="main-content h-screen flex justify-center items-center">
+        <img class="w-1/4 h-auto" src="../assets/workinprogress.png" style="margin-bottom: 150px;">
+      </div>
     </div>
   </div>
-    <div class="main-content h-screen" style="display: flex; flex-direction: column; justify-content: center; align-items: center; max-width: 800px; margin: auto;">
-    </div>
-    </div>
 </template>
 
 <script>
 import sidebar from '../components/sidebar.vue';
 
 export default {
-    name: 'messagerie',
-    components: {
-        sidebar,
-    },
+  name: 'messagerie',
+  components: {
+      sidebar,
+  },
 };
 </script>
 
--- src/vue/notes.vue
+++ src/vue/notes.vue
@@ -1,14 +1,15 @@
 <template>
-    <sidebar :activeButton="'notes'"/>
-    <div class="bg-blue-200 flex">
-        <div class="flex flex-col ml-80">
-    <div class="flex bg-white text-[#2176FF] w-[70vw] h-[56px] ml-[50px] rounded-lg justify-between items-center mt-10 py-16">
-      <h1 class="search-title text-[#2176FF] text-5xl font-bold flex items-center ml-10">Mes Notes</h1>
+  <div class="bg-blue-200 flex">
+    <sidebar :activeButton="'messagerie'"/>
+    <div class="flex flex-col ml-80 flex-grow">
+      <div class="flex bg-white text-[#2176FF] w-[70vw] h-[56px] ml-[50px] rounded-lg justify-between items-center mt-10 py-16">
+        <h1 class="search-title text-[#2176FF] text-5xl font-bold flex items-center ml-10">Mes notes</h1>
+      </div>
+      <div class="main-content h-screen flex justify-center items-center">
+        <img class="w-1/4 h-auto" src="../assets/workinprogress.png" style="margin-bottom: 150px;">
+      </div>
     </div>
   </div>
-    <div class="main-content h-screen" style="display: flex; flex-direction: column; justify-content: center; align-items: center; max-width: 800px; margin: auto;">
-    </div>
-    </div>
 </template>
 
 <script>
