b91d822b9c92903626d072761f9117ff401c68a1
Aymteb <ayman@digitaledge.studio>
2024-04-21 19:41:29 +0100

    [fix] active button

--- src/vue/notes.vue
+++ src/vue/notes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-blue-200 flex">
-    <sidebar :activeButton="'messagerie'"/>
+    <sidebar :activeButton="'notes'"/>
     <div class="flex flex-col ml-80 flex-grow">
       <div class="flex bg-white text-[#2176FF] w-[70vw] h-[56px] ml-[50px] rounded-lg justify-between items-center mt-10 py-16">
         <h1 class="search-title text-[#2176FF] text-5xl font-bold flex items-center ml-10">Mes notes</h1>
